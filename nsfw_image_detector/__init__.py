87cd6bac49277871c2132c9893495afa10b595a2
CabreraJimenez <ccabrera@freepik.com>
2025-05-05 14:37:36 +0200

    Change version

--- nsfw_image_detector/__init__.py
+++ nsfw_image_detector/__init__.py
@@ -4,5 +4,5 @@ NSFW Image Detector - A Python package for NSFW content detection using EVA-base
 
 from .detector import NSFWDetector, NSFWLevel
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = ["NSFWDetector", "NSFWLevel"] 
